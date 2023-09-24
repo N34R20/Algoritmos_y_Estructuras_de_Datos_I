6366ac4c831e87886fa7bc78155fbf8896813a60
N34R20 <francisco.olmos.99@gmail.com>
2023-09-24 16:55:56 -0300

    sumando ejercicios guia 4 y 5

--- Haskell/resolucion_guia_4.hs
+++ Haskell/resolucion_guia_4.hs
@@ -55,16 +55,38 @@ cantDigitos n | n < 10 = 1
 
 iesimoDigitoSinRecursion :: Integer -> Integer -> Integer
 iesimoDigitoSinRecursion n i | i > cantDigitos n = 0 
-                             | otherwise = mod (div n (10**(i-1))) 10
+                             | otherwise = mod (div n (10^(i-1))) 10
 
 -- Ejercicio 9
 -- Especiﬁcar e implementar una funcion esCapicua :: Integer ->Bool que dado n ∈ N≥0 determina si n es
 -- un numero capicua
 
 -- requiere {neN>=0}
-esCapicua :: Integer -> Bool 
-esCapicua n | n < 10 = True
-            | otherwise = esCapicua(div n 10**cantDigitos n - 1 == mod n 10) = True
+-- esCapicua :: Integer -> Bool 
+-- esCapicua n | n < 10 = True
+--            | otherwise = esCapicua(div n 10**cantDigitos n - 1 == mod n 10) = True
+
+-- Ejercicio 10
+--a) 
+f1 :: Int -> Int
+f1 n | n == 0 = 1
+         | otherwise = 2^n + f1(n-1)
+
+--b)
+f2 :: (Int, Float) -> Float
+f2 (n,q)| n == 0 = 1
+            | otherwise = q^n + f2(n-1, q)
+
+--c)
+f3 :: (Int, Float) -> Float
+f3 (n,q)| n == 0 = 1
+        | otherwise = q^(2*n) + f2((2*n)-1, q) 
+
+--d)
+f4 :: (Int, Float) -> Float
+f4 (n,q)| n == n = q^n
+        | otherwise = q^(2*n) + f2((2*n)-1, q) 
+-- se puede hacer recursion a la vez?
 
 -- Ejercicio 11
 -- a) 
@@ -75,10 +97,17 @@ factorial n | n == 0 = 1
 
 eAprox :: Integer -> Float
 eAprox n | n == 0 = 1.0
-         | n == 1 = 1.0
+-- | n == 1 = 1.0
          | otherwise = 1/fromIntegral((factorial n)) + eAprox (n-1)
 
 
+-- Ejercicio 12
+sucesion2 :: Int -> Float
+sucesion2 n | n == 1 = 2
+            | otherwise = 2 + (1/(sucesion2(n-1))) 
+
+raizDe2Aprox :: Int -> Float
+raizDe2Aprox n = sucesion2 n - 1  
 
 
 -- Ejercicio 13
@@ -88,7 +117,28 @@ sumatoriaInterna n m = n^m + sumatoriaInterna n (m-1)
 
 sumatoriaDoble :: Int -> Int -> Int
 sumatoriaDoble 0 _ = 0
-sumatoriaDoble n m = sumatoriaDoble n m + sumatoriaDoble (n-1) m
+sumatoriaDoble n m = sumatoriaInterna n m + sumatoriaDoble (n-1) m
+
+
+-- Ejercicio 14
+sumaPotencias :: Integer -> Integer -> Integer -> Integer
+sumaPotencias q n 1 = q^(n+1) 
+sumaPotencias q 1 m = q^(1+m)
+sumaPotencias q n m = q^2 + q^(m+n) + sumaPotencias q (n-1) m + sumaPotencias q n (m-1) 
+
+-- Ejercicio 15
+
+sumaRacionalesInterna :: Integer -> Integer -> Float
+sumaRacionalesInterna 0 _ = 0
+sumaRacionalesInterna _ 0 = 0
+sumaRacionalesInterna p q = fromIntegral (p) / fromIntegral (q) + sumaRacionalesInterna p (q - 1)
+
+sumaRacionalesExterna :: Integer -> Integer -> Float
+sumaRacionalesExterna _ 0 = 0
+sumaRacionalesExterna 0 _ = 0
+sumaRacionalesExterna p q = sumaRacionalesInterna p q + sumaRacionalesExterna (p - 1) q
+
+
 
 -- Ejercicio 16
 -- a)
@@ -100,8 +150,40 @@ buscarDivisor n k | k*k > n = n
 menorDivisor :: Integer -> Integer
 menorDivisor n = buscarDivisor n 2
 
+--b)
+esPrimo :: Integer -> Bool
+esPrimo 1 = True
+esPrimo n | menorDivisor n == n = True 
+          | otherwise = False
+
+--c)
+sonCoprimos :: Integer -> Integer -> Bool
+sonCoprimos n m | esPrimo n && esPrimo m = True
+                | otherwise = False
+
+--d)
+contarPrimos :: Integer -> Integer  
+contarPrimos n | n == 1 = 1
+               | otherwise = contarPrimos (n-1)  
+
+--nEsimoPrimo :: Integer -> Integer
+--nEsimoPrimo n | n == 1 = 1
+--              | otherwise = contarPrimos n 1 + nEsimoPrimo (n-1)  
+
 -- ejercicio 17
 -- no funca
+
 esFibonacci :: Integer -> Bool
-esFibonacci n | n == 0 = True
-              | otherwise = (n == fibonacci(n) || esFibonacci(n-1))
+esFibonacci n | n == 1 = True
+              | otherwise = False 
+
+-- Ejercicio 18
+
+
+-- Ejercicio 19
+
+
+-- Ejercicio 20
+
+
+-- Ejercicio 21
--- Haskell/resolucion_guia_5.hs
+++ Haskell/resolucion_guia_5.hs
@@ -1,10 +1,11 @@
--- Ejercicio 1
+{-- Ejercicio 1 --} 
+
 -- 1)
 longitud :: (Eq t) => [t] -> Integer
 longitud [] = 0
 longitud s = 1 + longitud(tail s)  
 
---2)
+-- 2)
 ultimo :: (Eq t) => [t] -> t
 ultimo s | longitud s == 1 = head(s) 
          | otherwise = ultimo(tail s) 
@@ -18,7 +19,7 @@ reverso :: (Eq t) =>[t] -> [t]
 reverso [] = []
 reverso s = reverso (quitar(head s) s) ++ [head s]
 
--- Ejercicio 2
+{-- Ejercicio 2 --}
 
 -- 1)
 pertenece :: (Eq t) => t -> [t] -> Bool 
@@ -30,6 +31,11 @@ todosIguales :: (Eq t) => [t] -> Bool
 todosIguales [] = False
 todosIguales s | head(s) == head(tail s) = True
                | otherwise = todosIguales (quitar (head s) s)
+
+-- 3) 
+todosDistintos :: (Eq t) => [t] -> Bool
+todosDistintos [] = True
+todosDistintos s = not (pertenece (head s) (tail s)) || todosDistintos(tail s)
 
 -- 4)
 hayRepetidos :: (Eq t) => [t] -> Bool 
@@ -48,8 +54,42 @@ quitarTodos _ [] = []
 quitarTodos n (x:xs) | n == x = quitarTodos n xs 
                      | otherwise = x:quitarTodos n (quitar n xs)       
 
+-- 7)
+eliminarRepetidos :: (Eq t) => [t] -> [t]
+eliminarRepetidos [] = []
+eliminarRepetidos (x:xs) = x:eliminarRepetidos (quitarTodos x xs) 
+
+
+-- 8) 
+chequearLongitud :: (Eq t) => [t] -> [t] -> Bool
+chequearLongitud [] [] = True
+chequearLongitud xs ys | longitud (eliminarRepetidos xs) == longitud (eliminarRepetidos ys) = True 
+                       | otherwise = False
+
+mismosElementos :: (Eq t) => [t] -> [t] -> Bool
+mismosElementos [] [] = True
+mismosElementos (x:xs) (y:ys) = chequearLongitud (x:xs) (y:ys) && mismosElementos xs ys
+--mismosElementos (x:xs) (y:ys) = chequearLongitud (quitarTodos x xs) (quitarTodos x ys)  
+
+
+-- 9)
+capicua :: (Eq t) => [t] -> Bool
+capicua (x:xs) | reverso (x:xs) == (x:xs) = True
+                      | otherwise = False
+
 
 -- Ejercicio 3
+
+-- 1) 
+sumatoria :: [Integer] -> Integer
+sumatoria (x:xs) | longitud (x:xs) == 1 = x
+                 | otherwise = x + sumatoria xs
+
+-- 2)
+productoria :: [Integer] -> Integer
+productoria (x:xs) | longitud (x:xs) == 1 = x
+                   | otherwise = x * productoria xs
+
 -- 3)
 mayor :: Integer -> Integer -> Integer
 mayor x y | x >= y = x
@@ -59,29 +99,105 @@ maximo :: [Integer] -> Integer
 maximo (x:[]) = x 
 maximo (x:xs) = mayor x (maximo xs)
 
+menor :: Integer -> Integer -> Integer
+menor x y | x <= y = x
+          | otherwise = y
+
+minimo :: [Integer] -> Integer
+minimo (x:[]) = x 
+minimo (x:xs) = menor x (minimo xs)
+
+-- 4)
+sumarN :: Integer -> [Integer] -> [Integer] 
+sumarN n [] = []
+sumarN n (x:xs) = [x+n] ++ sumarN n xs
+
+-- 5) 
+primero :: [Integer] -> Integer
+primero xs = head(xs)
+
+sumarElPrimero :: [Integer] -> [Integer]
+sumarElPrimero [] = []
+sumarElPrimero (x:xs) = sumarN x (x:xs)
+
+-- 6) 
+sumarElUltimo :: [Integer] -> [Integer]
+sumarElUltimo [] = []
+sumarElUltimo (x:xs) = sumarN (last xs) (x:xs)
+
+-- 7)
+esPar :: Integer -> [Integer]
+esPar a | mod a 2 == 0 = [a]
+        | otherwise = []
+
+pares :: [Integer] -> [Integer]
+pares [] = []
+pares (x:xs) = esPar x ++ pares xs 
+
+-- 8)
+esMultiplo :: Integer -> Integer -> [Integer]
+esMultiplo a n | mod a n == 0 = [a]
+               | otherwise = []
+
+multiplosDeN :: Integer -> [Integer] -> [Integer] 
+multiplosDeN _ [] = []
+multiplosDeN n (x:xs) = esMultiplo x n ++ multiplosDeN n xs
 
 -- 9) 
-ordenar :: [Integer] -> [Integer]
-ordenar [] = []
-ordenar s = ordenar (quitar (maximo s) s) ++ [maximo s]
+ordenar1 :: [Integer] -> [Integer]
+ordenar1 [] = []
+ordenar1 s = ordenar1 (quitar (maximo s) s) ++ [maximo s]
+
+ordenar2 :: [Integer] -> [Integer]
+ordenar2 [] = []
+ordenar2 s = [minimo s] ++ ordenar2 (quitar (minimo s) s) 
+
+
+-- Ejercicio 4
+
+-- 1) 
+-- sacarBlancosRepetidos :: [Char] -> [Char]
+-- sacarBlancosRepetidos [] = []
+-- sacarBlancosRepetidos 
+
 
 -- Ejercicio 5
 
+-- 1)
+
+sumatoriaHastaN :: Integer -> [Integer] -> Integer
+sumatoriaHastaN n (x:xs) | longitud (x:xs) == n = x
+                         | otherwise = x + sumatoriaHastaN n xs
+
+sumaAcumulada :: [Integer] -> [Integer]
+sumaAcumulada [] = [] 
+sumaAcumulada s = sumaAcumulada (quitar (ultimo s) s) ++ [sumatoriaHastaN (longitud s) s]
+
+
+
 -- 2)
-esPrimo :: Integer -> Bool
-esPrimo 1 = False
-esPrimo n = menorDivisor x == x
+-- buscarDivisor :: Integer -> Integer -> Integer
+-- buscarDivisor n k | k*k > n = n 
+--                   | mod n k == 0 = k 
+--                   | otherwise = buscarDivisor n (k+1)
+
+-- menorDivisor :: Integer -> Integer
+-- menorDivisor n = buscarDivisor n 2
+
+-- esPrimo :: Integer -> Bool
+-- esPrimo 1 = False
+-- esPrimo n = (menorDivisor n) == n
 
 
-primosDivisores :: Integer -> Integer -> [Integer]
-primosDivisores d n | esPrimo n = [n]
-                    | esPrimo d && mod n d == 0 = d:primosDivisores d div(n d)
-                    | otherwise = primosDivisores (d+1) n 
+-- primosDivisores :: Integer -> Integer -> [Integer]
+-- primosDivisores d n | esPrimo n = [n]
+--                     | esPrimo d && mod n d == 0 = d:primosDivisores d div(n d)
+--                     | otherwise = primosDivisores (d+1) n 
 
-descomponer :: Integer -> [Integer]
-descomponer n = primosDivisores 2 n 
+-- descomponer :: Integer -> [Integer]
+-- descomponer n = primosDivisores 2 n 
 
 
-descomponerEnPrimos :: [Integer] -> [[Integer]]
-descomponerEnPrimos [] = [[]]
-descomponerEnPrimos (x:xs) = descomponer x ++ descomponerEnPrimos xs
+-- descomponerEnPrimos :: [Integer] -> [[Integer]]
+-- descomponerEnPrimos [] = [[]]
+-- descomponerEnPrimos (x:xs) = descomponer x ++ descomponerEnPrimos xs
