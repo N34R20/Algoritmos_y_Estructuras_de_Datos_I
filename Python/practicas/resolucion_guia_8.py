2d8c8888b3984284462c474bac40e8682706a73a
N34R <francisco.olmos.99@gmail.com>
2023-10-27 02:21:54 -0300

    update p8

--- Python/practicas/resolucion_guia_8.py
+++ Python/practicas/resolucion_guia_8.py
@@ -1,6 +1,6 @@
 from queue import Queue as Cola
 from queue import LifoQueue as Pila
-
+import random
 
 """
 ARCHIVOS
@@ -8,7 +8,7 @@ ARCHIVOS
 
 # 1
 
-def contar_lineas(nombre_archivo: str) -> int:
+def contarLineas(nombre_archivo: str) -> int:
     contador = 0 
     with open(nombre_archivo, "r", encoding="utf-8") as file:
         lineas = file.readlines()
@@ -17,9 +17,8 @@ def contar_lineas(nombre_archivo: str) -> int:
 
     return contador 
 
-print("La cantidad de lineas que tiene son:", contar_lineas("/home/clinux01/Descargas/python/texto.txt"))
 
-def existe_palabra(palabra: str, nombre_archivo: str) -> bool:
+def existePalabra(palabra: str, nombre_archivo: str) -> bool:
     with open(nombre_archivo, "r", encoding="utf-8") as file:
         lineas = file.readlines()
         for line in lineas:
@@ -28,10 +27,9 @@ def existe_palabra(palabra: str, nombre_archivo: str) -> bool:
         else:
             return False
 
-print(existe_palabra("pelotudo","/home/clinux01/Descargas/python/texto.txt"))
 
 
-def cantidad_apariciones(palabra_buscada: str, nombre_archivo: str) -> int:
+def cantidadApariciones(palabra_buscada: str, nombre_archivo: str) -> int:
     with open(nombre_archivo, "r", encoding="utf-8") as file:
         lineas = file.readlines()
         apariciones = 0
@@ -46,12 +44,11 @@ def cantidad_apariciones(palabra_buscada: str, nombre_archivo: str) -> int:
     
     return apariciones
 
-print(cantidad_apariciones("pelotudo","/home/clinux01/Descargas/python/texto.txt"))
 
 
 # 2
 
-def clonar_sin_comentarios(nombre_archivo: str) -> None:
+def clonarSinComentarios(nombre_archivo: str) -> None:
     lineas_sin_comentario: list[str] = list()
     with open(nombre_archivo, "r", encoding="utf-8") as file:
         lineas: list[str] = file.readlines()
@@ -65,7 +62,7 @@ def clonar_sin_comentarios(nombre_archivo: str) -> None:
 
 # 3
 
-def escribir_en_reverso(nombre_archivo):
+def escribirEnReverso(nombre_archivo):
     copia_archivo_reverso = list()
     with open(nombre_archivo, "r", encoding="utf-8") as file:
         lineas = file.readlines()
@@ -75,19 +72,46 @@ def escribir_en_reverso(nombre_archivo):
             copia_archivo_reverso = [value] + copia_archivo_reverso
     print(copia_archivo_reverso)
 
-escribir_en_reverso("/home/clinux01/Descargas/python/texto.txt")
-
 
 # 4
+
+def agregarFraseAlFinal(nombre_archivo: str, frase: str) -> None:
+    with open(nombre_archivo, "a", encoding="utf-8") as file:
+        file.write(frase)
+    return None
 
 
 # 5
 
+def agregarFraseAlPrincipio(nombre_archivo: str, frase: str) -> None:
+    with open(nombre_archivo, 'a', encoding='utf-8') as file:
+    # Move the file pointer to the beginning of the file
+        file.seek(0)
+    # Write the new data to the file
+        file.write(frase+"\n")
+
+return None
 
 # 6
 
+def binarioMasLegibles(nombre_archivo: str) -> List():
+    palabras_legibles = list()
 
-# 7
+    with open(nombre_archivo, 'rb', encoding='utf-8') as file:
+        lineas = file.readlines()
+    return  palabras_legibles
+
+# 7 
+def inspect_column(data: list, num_of_column: int) -> list:
+  list_ = list()
+  for line in data[1::]:
+    list_.append(line.split(",")[num_of_column].replace("\n", "").strip())
+  return list_
+
+def promedioEstudiante(lu: str) -> float:
+    with open(nombre_archivo, 'r', encoding='utf-8') as file:
+        lineas = file.readlines()
+    return 
 
 """
 PILAS
@@ -95,7 +119,22 @@ PILAS
 
 # 8
 
+def generar_numeros_random(n:int,desde:int,hasta:int) ->Pila:
+    p=Pila()
+    for i in range(n):
+        p.put(random.randint(desde, hasta))
+        
+    return p
+
 # 9
+
+def cantidad_elementos(p:Pila)-> int:
+    elementos:int= 0 
+    while not p.empty():
+        p.get()
+        elementos+=1
+    return elementos
+
 
 # 10
 
@@ -127,6 +166,7 @@ def buscarElMaximo (p:Cola) -> int:
         value=max(next_value,value)
     return value
 
+"""
 pila: Cola=Cola()
 pila.put(1)
 pila.put(4)
@@ -134,10 +174,45 @@ pila.put(-3)
 print(list(pila.queue))
 print(buscarElMaximo(pila))
 print(list(pila.queue))
-
+"""
 
 
 # 11
+
+def chequear_parejas(lista):
+    
+    if "(" not in lista or ")" not in lista:
+        
+        return lista
+    
+    else:
+        lista.remove("(")
+        lista.remove(")")
+        print(lista)
+        chequear_parejas(lista)
+
+    return lista    
+
+
+def esta_bien_balanceada(formula:str) -> bool:
+    
+    caracteres_balanceados = list()
+    for char in formula:
+        if char == '(' or char == ')':
+            caracteres_balanceados.append(char)
+
+    if len(caracteres_balanceados)%2 == 0 and caracteres_balanceados[0] == "(" and caracteres_balanceados[-1] == ")":
+        nueva = chequear_parejas(caracteres_balanceados)
+
+        if nueva == ["(",")"] or len(nueva) == 0:
+            return True 
+        else: 
+            return False    
+
+    else: 
+            return False        
+    
+print(esta_bien_balanceada("(())()"))
 
 # 12
 
